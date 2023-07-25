4eaf8d5be4c5d9f7cb072d95f9b4bd5c2b48822a
swdevlin <swdevlin@gmail.com>
2023-07-24 22:37:28 -0400

    d3 and d2

--- dice.js
+++ dice.js
@@ -11,6 +11,14 @@ const d6 = () => {
     return rng.die(6);
 }
 
+const d3 = () => {
+  return rng.die(3);
+}
+
+const d2 = () => {
+  return rng.die(2);
+}
+
 const fluxRoll = () => {
   return d6() - d6();
 }
@@ -28,5 +36,7 @@ module.exports = {
   twoD6: twoD6,
   fluxRoll: fluxRoll,
   d6: d6,
+  d3: d3,
+  d2: d2,
   ROLL_CACHE: ROLL_CACHE,
 }
