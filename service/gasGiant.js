8fe644dc8c40f3e07764b0215a5232ecd270057c
Shawn <swdevlin@hey.com>
2026-01-29 20:15:25 -0500

    Fix size calculation

--- service/gasGiant.js
+++ service/gasGiant.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { GasGiant } = require('../gasGiants');
-const { SOL_DIAMETER } = require('../utils');
+const { EARTH_DIAMETER} = require('../utils');
 const { d6, threeD6, twoD6 } = require('../dice');
 const { Random } = require('random-js');
 const r = new Random();
@@ -12,11 +12,11 @@ router.get('/', (req, res) => {
 
     let gg;
     if (size === 'GS') {
-        gg = new GasGiant(size, SOL_DIAMETER * (r.die(3) + r.die(3)), r.integer(2, 7) * 5);
+        gg = new GasGiant(size, EARTH_DIAMETER * (r.die(3) + r.die(3)), r.integer(2, 7) * 5);
     } else if (size === 'GM') {
-        gg = new GasGiant(size, SOL_DIAMETER * (d6() + 6), 20 * (threeD6() - 1));
+        gg = new GasGiant(size, EARTH_DIAMETER * (d6() + 6), 20 * (threeD6() - 1));
     } else if (size === 'GL') {
-        gg = new GasGiant(size, SOL_DIAMETER * (twoD6() + 6), r.die(3) * 50 * (threeD6() + 4));
+        gg = new GasGiant(size, EARTH_DIAMETER * (twoD6() + 6), r.die(3) * 50 * (threeD6() + 4));
     } else {
         req.logger.error(`Invalid size requested: ${size}`, { tenant });
         return res.status(400).json({ error: "Invalid size. Use GS, GM, or GL." });
