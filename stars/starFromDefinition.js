bbbd32c3da06a284c8649f274b77415802933d93
Shawn W. Devlin <swdevlin@gmail.com>
2026-02-22 14:54:55 -0500

    Support star names

--- stars/starFromDefinition.js
+++ stars/starFromDefinition.js
@@ -1,17 +1,22 @@
-const predefinedClassification = require("./predefinedClassification");
-const Star = require("./star");
-const {ORBIT_TYPES, companionOrbit, calculatePeriod} = require("../utils");
+const predefinedClassification = require('./predefinedClassification');
+const Star = require('./star');
+const { ORBIT_TYPES, companionOrbit, calculatePeriod } = require('../utils');
 
 const starFromDefinition = (definition, orbitType, unusualChance) => {
   const classification = predefinedClassification(definition);
   const star = new Star(classification, orbitType);
+  star.name = definition.name;
   if (definition.companion) {
-    const companion = starFromDefinition(definition.companion, ORBIT_TYPES.COMPANION, unusualChance);
+    const companion = starFromDefinition(
+      definition.companion,
+      ORBIT_TYPES.COMPANION,
+      unusualChance
+    );
     star.companion = companion;
     companion.orbit = companionOrbit();
     companion.period = calculatePeriod(companion, star);
   }
   return star;
-}
+};
 
 module.exports = starFromDefinition;
