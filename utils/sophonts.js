e3a1f10fbf952e58f6b703a43fdfda369a076b4d
Shawn W. Devlin <swdevlin@gmail.com>
2026-03-17 12:49:18 -0400

    Use core rules for sophont checks

--- utils/sophonts.js
+++ utils/sophonts.js
@@ -1,26 +1,19 @@
-const {twoD6, percentageChance} = require("../dice");
+const { twoD6 } = require('../dice');
 
 const nativeSophont = (star, planet) => {
-  if (planet.biocomplexityCode < 7)
-    return false;
-  let r= twoD6() + Math.min(planet.biocomplexityCode, 9) - 7;
-  const found = r >= 13;
-  const chance = parseFloat(process.env.nativeSophontChance);
-  return found && percentageChance(chance);
-}
+  if (planet.biocomplexityCode < 7) return false;
+  let r = twoD6() + Math.min(planet.biocomplexityCode, 9) - 7;
+  return r >= 13;
+};
 
 const extinctSophont = (star, planet) => {
-  if (planet.biocomplexityCode < 7)
-    return false;
-  let r= twoD6() + Math.min(planet.biocomplexityCode, 9) - 7;
-  if (star.age > 5)
-    r++;
-  const found = r >= 13;
-  const chance = parseFloat(process.env.extinctSophontChance);
-  return found && percentageChance(chance);
-}
+  if (planet.biocomplexityCode < 7) return false;
+  let r = twoD6() + Math.min(planet.biocomplexityCode, 9) - 7;
+  if (star.age > 5) r++;
+  return r >= 13;
+};
 
 module.exports = {
   nativeSophont: nativeSophont,
   extinctSophont: extinctSophont,
-}
+};
